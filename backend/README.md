2cb8f9d3e9d96a9c7e0cd17b29aed0dd788916da
Takatoshi Ono <takatoshi.ono@gmail.com>
2020-10-09 08:53:36 +0900

    add README for backend

---
+++ backend/README.md
@@ -0,0 +1,27 @@
+# Backend
+
+Backend is a backend of kakeibo.
+
+## `mf` Command
+
+Manage data of Money Forward ME.
+
+```
+Usage:
+  mf [command]
+
+Available Commands:
+  csv         Download csv files from Money Forward ME
+  db          Import files to database
+  drive       Upload and download files to Google Drive
+  help        Help about any command
+
+Flags:
+      --config string   config file (default is $HOME/.mf.yaml)
+  -h, --help            help for mf
+  -t, --toggle          Help message for toggle
+```
+
+## API
+
+TBD
--- backend/internal/cmd/mf/root.go
+++ backend/internal/cmd/mf/root.go
@@ -31,13 +31,8 @@ func init() {
 func NewCmd(o *Option) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "mf",
-		Short: "A brief description of your application",
-		Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Short: "Manage data of Money Forward ME",
+		Long:  `Manage data of Money Forward ME.`,
 		// Uncomment the following line if your bare application
 		// has an action associated with it:
 		//	Run: func(cmd *cobra.Command, args []string) { },
